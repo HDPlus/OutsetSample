1788f76954dece64e3faaae17a0881cd6c9efae9
Dmitriy Smishniy <dsmishniy@kaaiot.io>
2020-01-23 16:53:34 +0200

    Add 'close/open' function for three windows on main dashboard

--- app/src/main/java/com/dsmishniy/outsetsample/presentation/MainActivity.kt
+++ app/src/main/java/com/dsmishniy/outsetsample/presentation/MainActivity.kt
@@ -2,12 +2,29 @@ package com.dsmishniy.outsetsample.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import com.dsmishniy.outsetsample.R
+import kotlinx.android.synthetic.main.main_dashboard.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        switch_cards_btn.setOnClickListener {
+            if(info_card.isVisible && info_items.isVisible) {
+                info_card.visibility = View.GONE
+                info_items.visibility = View.GONE
+                hidden_cards.visibility = View.VISIBLE
+                switch_cards_btn.setImageResource(R.drawable.ic_open_cards)
+            } else {
+                info_card.visibility = View.VISIBLE
+                info_items.visibility = View.VISIBLE
+                hidden_cards.visibility = View.GONE
+                switch_cards_btn.setImageResource(R.drawable.ic_close_cards)
+            }
+        }
     }
 }
