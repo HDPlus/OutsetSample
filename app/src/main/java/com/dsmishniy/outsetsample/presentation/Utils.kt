812052fb43e1cdd874c59a38a9808a105a2f02b9
Dmitriy Smishniy <dsmishniy@kaaiot.io>
2020-01-27 14:57:33 +0200

    Added top shadow line. Moved down clock

--- app/src/main/java/com/dsmishniy/outsetsample/presentation/Utils.kt
+++ app/src/main/java/com/dsmishniy/outsetsample/presentation/Utils.kt
@@ -1,5 +1,6 @@
 package com.dsmishniy.outsetsample.presentation
 
+import android.content.res.Resources
 import android.content.res.TypedArray
 import android.widget.TextView
 import com.dsmishniy.outsetsample.R
@@ -11,3 +12,6 @@ fun TextView.setTextColorFromAttr(styleableId: Int, attrs: TypedArray) {
     val color =context.resources.getColor(attrs.getResourceId(styleableId, R.color.colorDark))
     this.setTextColor(color)
 }
+
+val Int.dp: Int
+    get() = (this / Resources.getSystem().displayMetrics.density).toInt()
